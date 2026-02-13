cfff287f8084e89ad5db3dc0b56c7b723c06f2c2
s.maysky <meg.work.996@gmail.com>
2026-02-13 08:27:21 +0300

    WIP AI integration

--- src/bot.ts
+++ src/bot.ts
@@ -4,10 +4,13 @@ import { ENounWords } from './utils/ENounWords.js';
 import { calcDaysTo } from './utils/calcDaysTo.js';
 import { SPRING_DATE } from './const.js';
 import { scheduleAction } from './utils/scheduleAction.js';
+import { Menu } from "@grammyjs/menu";
+import OpenAI from "openai";
 
 export async function startBot() {
+    const client = new OpenAI();
+
     const bot = new Bot(process.env.TG_API_KEY!);
-    bot.start();
 
     const now = dayjs();
     const nextDay = now.clone().hour(0).minute(0).second(0).millisecond(0).add(1, 'day');
@@ -25,5 +28,19 @@ export async function startBot() {
         const days = calcDaysTo(dayjs(SPRING_DATE));
         bot.api.sendMessage(process.env.CHAT_ID!, `До весны осталось ${days} ${ENounWords.getNoun(days, ENounWords.Day)}.`);
     }
+
+    bot.command('daily_news', async (ctx) => {
+        const chatInfo = await ctx.getChat();
+        // ctx.mem
+        console.log('chatInfo: ', chatInfo);
+
+        const response = await client.responses.create({
+            model: 'gpt-4-turbo',
+            input: 'Это тестовый запрос, отвечай одним словом.',
+        });
+
+        await ctx.reply(response.output_text);
+    });
+
+    bot.start();
 }
- 
