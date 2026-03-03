c5f8591cd170ab2a16900a48abf13a647455fa59
s.maysky <meg.work.996@gmail.com>
2026-03-03 18:00:48 +0300

    fix express test

--- index.ts
+++ index.ts
@@ -3,9 +3,10 @@ import 'reflect-metadata';
 import './src/db/index.js';
 
 import { PartyBot } from './src/PartyBot.js';
+import { PartyBotAPI } from './src/PartyBotAPI.js';
 
 function main() {
-    new PartyBot();
+    new PartyBotAPI(new PartyBot());
 }
 
 main();
