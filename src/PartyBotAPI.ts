0d03b30646204f8811205f4091541184742a35ea
s.maysky <meg.work.996@gmail.com>
2026-03-03 17:32:07 +0300

    express test

---
+++ src/PartyBotAPI.ts
@@ -0,0 +1,22 @@
+import type { PartyBot } from './PartyBot.js';
+import express from 'express';
+
+export class PartyBotAPI {
+    private bot: PartyBot;
+
+    constructor(bot: PartyBot) {
+        this.bot = bot;
+
+        const app = express();
+        const port = 80;
+
+        app.post('/', (req, res) => {
+            res.send('Hello World!');
+        });
+
+        app.listen(port, () => {
+            console.log('Party Bot API');
+            console.log(`Start listening on ${port}`);
+        });
+    }
+}
