0da7226b40232ff8f1914edb2e82f1408bd5fc5d
s.maysky <meg.work.996@gmail.com>
2026-03-07 16:40:21 +0300

    added api send message

--- src/PartyBotAPI.ts
+++ src/PartyBotAPI.ts
@@ -1,6 +1,11 @@
 import type { PartyBot } from './PartyBot.js';
 import express from 'express';
 
+interface APISendMessageBody {
+    tg_api_key?: string;
+    msg?: string;
+}
+
 export class PartyBotAPI {
     private bot: PartyBot;
 
@@ -8,19 +13,31 @@ export class PartyBotAPI {
         this.bot = bot;
 
         const app = express();
-        const port = 3000;
+        app.use(express.json());
 
-        app.post('/', (req, res) => {
-            res.send('Hello World!');
-        });
-
-        app.get('/', (req, res) => {
-            res.send('Это GET детка по HTTPS!');
-        });
-
-        app.listen(port, '0.0.0.0', () => {
+        app.listen(3000, '0.0.0.0', () => {
             console.log('Party Bot API');
-            console.log(`Start listening on ${port}`);
+            console.log(`Start listening on 3000`);
+        });
+
+        app.post('/send-message/', async (req, res) => {
+            try {
+                const { tg_api_key, msg } = req.body as APISendMessageBody;
+
+                if (!msg || !tg_api_key) {
+                    return res.status(400).json({ success: false, message: 'props required' });
+                }
+
+                if (tg_api_key !== process.env.TG_API_KEY!) {
+                    return res.status(400).json({ success: false, message: 'wrong tg api key' });
+                }
+
+                const result = await this.bot.sendMessage(msg);
+                res.status(200).json({ success: true, info: result });
+            } catch (e) {
+                console.error('Failed to send message: ', e);
+                res.status(500).json({ success: false, message: 'something went wrong' });
+            }
         });
     }
 }
